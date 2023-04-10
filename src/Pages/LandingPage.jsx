68a6e1eef26b45c6df10ee56354865b1f937386f
Manuekle <elustondo129@gmail.com>
2023-04-10 18:00:14 -0500

    button

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,9 +5,19 @@ function LandingPage() {
     <div className="grid grid-cols-2 gap-4">
       <div className="col-span-1 bg-black p-8">
         <h1 className="text-white font-md font-normal tracking-normal">
-          Mi Cuenta
+          My Expensive
         </h1>
         <hr className="border-zinc-800 border-1 my-2" />
+        {/* <h1 className="text-sm text-zinc-100">Categories</h1> */}
+        <button
+          type="button"
+          className="w-full flex flex-row justify-between items-center gap-2 border p-2 rounded-md"
+        >
+          <h1 className="text-white text-sm">Services</h1>
+          <button className="text-white rotate-90" type="button">
+            {'>'}
+          </button>
+        </button>
       </div>
     </div>
   );
